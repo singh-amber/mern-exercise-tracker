181033f4210e4d21df5f5e2b3cfe0c0f6043b348
singh-amber <ambersinghraghuvanshi@gmail.com>
2021-01-16 18:12:42 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -25,13 +25,6 @@ const usersRouter = require('./routes/users');
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
-
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('../build'))
-}
-
-
-
 app.listen(port, () => {
 	console.log(`Server is running on port: ${port}`);
 });
